b5a44cad41ef192a56afc777062b7d3a916af4c8
kozuke <abc.121o121@gmail.com>
2019-02-20 22:51:22 +0900

    create model

--- src/accountbook/admin.py
+++ src/accountbook/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ExpnsCatg, AccountBook
+
+class AccountBookAdmin(admin.ModelAdmin):
+    list_display=('sls_dt','expns_catg_nm','prc_amt','memo')
+
+admin.site.register(ExpnsCatg)
+admin.site.register(AccountBook,AccountBookAdmin)
--- src/accountbook/models.py
+++ src/accountbook/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class ExpnsCatg(models.Model):
+    class Meta:
+        db_table = "ExpnsCatg"
+
+    expns_catg_nm = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.expns_catg_nm
+
+
+class AccountBook(models.Model):
+    class Meta:
+        db_table = "AccountBook"
+
+    sls_dt = models.DateField(verbose_name="日付", default=datetime.now)
+    expns_catg_nm = models.ForeignKey(
+        ExpnsCatg, on_delete=models.PROTECT, verbose_name="カテゴリ")
+    prc_amt = models.IntegerField(verbose_name="金額", help_text="単位は日本円")
+    memo = models.CharField(verbose_name="メモ", max_length=500)
+
+    def __str__(self):
+        return self.memo
